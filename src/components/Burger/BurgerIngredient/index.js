74f7b55236027e56e91ee6d5fcee0cbe8cad715e
Bui Kiem <buivantrung1105@gmail.com>
2019-07-18 21:41:38 +0700

    Add PropTypes validation to BurgerIngredient component.

--- src/components/Burger/BurgerIngredient/index.js
+++ src/components/Burger/BurgerIngredient/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./BurgerIngredient.css";
 
 const burgerIngredient = (props) => {
@@ -33,6 +34,10 @@ const burgerIngredient = (props) => {
     }
 
     return ingredient;
+};
+
+burgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired,
 };
 
 export default burgerIngredient;
