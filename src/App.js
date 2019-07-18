224c1eb3f37e09670218ff6941636b7e5acd95c6
Bui Kiem <buivantrung1105@gmail.com>
2019-07-18 20:59:31 +0700

    Create the Layout Component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Layout from './components/Layout';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            <Layout>
+                <p>Testing...</p>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
