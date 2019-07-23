2a1efbf1b0df77daadc2b7dffb5d9eaa0c61ee2c
Bui Anh Huy <bahuy@tma.com.vn>
2019-07-23 16:36:56 +0700

    Reuse the backdrop to drawer.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Aux from "../../hoc/Aux";
 import Toolbar from "../Navigation/Toolbar";
@@ -6,14 +6,27 @@ import SideDrawer from "../Navigation/SideDrawer";
 
 import classes from "./Layout.module.css";
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: true,
+    };
+
+    closeSideDrawerHandler = () => {
+        this.setState({showSideDrawer: false})
+    };
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer show={this.state.showSideDrawer} backdropClick={this.closeSideDrawerHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/index.js
+++ src/components/Navigation/SideDrawer/index.js
@@ -1,22 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+import Aux from "../../../hoc/Aux";
+import Backdrop from "../../UI/Backdrop";
 import Logo from "../../Logo";
 import NavigationItems from "../Items";
 
 import classes from "./SideDrawer.module.css";
 
 const sideDrawer = (props) => {
+    const sideDrawerClasses = [classes.SideDrawer, props.show ? classes.Open : classes.Close];
 
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.show} clicked={props.backdropClick} />
+            <div className={sideDrawerClasses.join(' ')} >
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
+
     );
+};
+
+sideDrawer.propTypes = {
+    backdropClick: PropTypes.func.isRequired,
+    show: PropTypes.bool,
 };
 
 export default sideDrawer;
