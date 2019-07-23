e6fd1e5860e2bbc79fccc1eccf09a71270928879
Bui Kiem <buivantrung1105@gmail.com>
2019-07-23 19:51:54 +0700

    Send POST request with an order.

--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -6,6 +6,8 @@ import BuildControls from "../../components/Burger/BuildControls";
 import Modal from "../../components/UI/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary";
 
+import axios from "../../axios-orders";
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -69,7 +71,24 @@ class BurgerBuilder extends Component {
     };
 
     continueOrderHandler = () => {
-        alert("You clicked continue!!!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Customer name',
+                address: {
+                    street: 'Address street',
+                    zipCode: '00000',
+                    country: 'NeverLand',
+                },
+                email: 'customer@example.com',
+            },
+            deliveryMethod: 'economy',
+        };
+        axios.post("/orders.json", order)
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error));
+
     };
 
     render() {
