5afb619dc81fe5b5fc25934b80aef8ac7dfbf57d
Bui Kiem <buivantrung1105@gmail.com>
2019-07-23 19:17:29 +0700

    Change folder structure.

--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import classes from "./Modal.module.css";
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../Backdrop";
 
 const modal = (props) => (
