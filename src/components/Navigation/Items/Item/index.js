786e0b372335bfa874cb9eec5e1531dcf5871a96
Bui Anh Huy <bahuy@tma.com.vn>
2019-07-23 14:45:09 +0700

    Add reusable navigation item.

---
+++ src/components/Navigation/Items/Item/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import classes from "./Item.module.css"
+
+const item = (props) => (
+    <li className={classes.NavigationItem}>
+        <a href={props.link} className={props.active ? classes.active : null}>{props.children}</a>
+    </li>
+);
+
+item.propTypes = {
+    link: PropTypes.string,
+    active: PropTypes.bool,
+    children: PropTypes.node,
+};
+
+export default item;
