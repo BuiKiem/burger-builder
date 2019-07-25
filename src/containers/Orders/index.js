7c8fd40b9dbff7329ff20e7cfbb31b40a8d5e2ba
Bui Anh Huy <bahuy@tma.com.vn>
2019-07-25 14:01:18 +0700

    fetch orders from server.

--- src/containers/Orders/index.js
+++ src/containers/Orders/index.js
@@ -1,8 +1,32 @@
 import React, {Component} from "react";
 
 import Order from "../../components/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler";
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true,
+    };
+
+    componentDidMount() {
+        axios.get("/orders.json")
+            .then((response) => {
+                const fetchedData = [];
+                for (let key in response.data) {
+                    fetchedData.push({
+                        ...response.data[key],
+                        id: key});
+                }
+
+                this.setState({loading: false, orders: fetchedData});
+            })
+            .catch((error) => {
+                this.setState({loading: false});
+            })
+    }
+
     render() {
         return (
             <div>
@@ -13,4 +37,4 @@ class Orders extends Component {
     };
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
