f8b355d39d2d2a9e3c8ba280a2d2927c3aab517c
Bui Anh Huy <bahuy@tma.com.vn>
2019-07-25 13:51:27 +0700

    Implement navigation link.

--- src/components/Navigation/Items/Item/index.js
+++ src/components/Navigation/Items/Item/index.js
@@ -1,17 +1,17 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import PropTypes from "prop-types";
 
 import classes from "./Item.module.css"
 
 const item = (props) => (
     <li key={props.children} className={classes.NavigationItem}>
-        <a href={props.link} className={props.active ? classes.active : null}>{props.children}</a>
+        <NavLink activeClassName={classes.active} exact={props.exact} to={props.link}>{props.children}</NavLink>
     </li>
 );
 
 item.propTypes = {
     link: PropTypes.string,
-    active: PropTypes.bool,
     children: PropTypes.node,
 };
 
